4cd1761c64163b8dd07ee7043901667e7337ed5c
aleksandar.kgeorgiev <aleksandar.kgeorgiev@scalefocus.com>
2024-02-19 19:04:51 +0200

    Initial commit with new modules utilities and buildSrc with Dependency Management system in it.

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.sales.newsinshort"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sales.newsinshort"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.activityCompose)
+    implementation(platform(Dependencies.composeBom))
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeUiGraphics)
+    implementation(Dependencies.composeUiToolingPreview)
+    implementation(Dependencies.composeMaterial3)
+//    implementation(Dependencies.navigationCompose)
+//    implementation(Dependencies.lifecycleViewModelKtx)
+//    implementation(Dependencies.hiltNavigationCompose)
+//    androidTestImplementation(Dependencies.composeBom)
+//    androidTestImplementation(Dependencies.composeTestJUnit4)
+//    debugImplementation(Dependencies.composeUiTooling)
+//    debugImplementation(Dependencies.composeUiTestManifest)
+
+//    implementation(Dependencies.retrofit)
+//    implementation(Dependencies.gsonConverter)
+//    implementation(Dependencies.moshiConverter)
+//    implementation(Dependencies.moshi)
+//    implementation(Dependencies.okhttp)
+//    implementation(Dependencies.loggingInterceptor)
+
+    implementation(project(Modules.utilities))
+
+    implementation(Dependencies.hiltAndroid)
+    kapt(Dependencies.hiltCompiler)
+    kapt(Dependencies.hiltAndroidCompiler)
+}
+
+kapt {
+    correctErrorTypes = true
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,35 @@
+object Dependencies {
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val appcompat by lazy { "androidx.appcompat:appcompat:${Versions.appcompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.material}" }
+    val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntimeKtx}" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeBom by lazy { "androidx.compose:compose-bom:${Versions.composeBom}" }
+    val composeUi by lazy { "androidx.compose.ui:ui:" }
+    val composeUiGraphics by lazy { "androidx.compose.ui:ui-graphics:" }
+    val composeUiToolingPreview by lazy { "androidx.compose.ui:ui-tooling-preview" }
+    val composeMaterial3 by lazy { "androidx.compose.material3:material3" }
+    val navigationCompose by lazy { "androidx.navigation:navigation-compose:${Versions.navigationCompose}" }
+    val composeTestJUnit4 by lazy { "androidx.compose.ui:ui-test-junit4:" }
+    val composeUiTooling by lazy { "androidx.compose.ui:ui-tooling:${Versions.composeUiTooling}" }
+    val composeUiTestManifest by lazy { "androidx.compose.ui:ui-test-manifest:" }
+    val lifecycleViewModelKtx by lazy { "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleViewModelKtx}" }
+
+    val hiltAndroid by lazy { "com.google.dagger:hilt-android:${Versions.hilt}" }
+    val hiltAndroidCompiler by lazy { "com.google.dagger:hilt-android-compiler:${Versions.hiltAndroidCompiler}" }
+    val hiltCompiler by lazy { "androidx.hilt:hilt-compiler:${Versions.hiltCompiler}" }
+//
+//    val hiltNavigationCompose by lazy { "androidx.hilt:hilt-navigation-compose:${Versions.hiltNavigationCompose}" }
+//
+//    val retrofit by lazy { "com.squareup.retrofit2:retrofit:${Versions.retrofit}" }
+//    val gsonConverter by lazy { "com.squareup.retrofit2:converter-gson:${Versions.gsonConverter}" }
+//    val moshiConverter by lazy { "com.squareup.retrofit2:converter-moshi:${Versions.moshiConverter}" }
+//    val moshi by lazy { "com.squareup.moshi:moshi-kotlin:${Versions.moshi}" }
+//    val okhttp by lazy { "com.squareup.okhttp3:okhttp:${Versions.okhttp}" }
+//    val loggingInterceptor by lazy { "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}" }
+}
+
+object Modules {
+    const val utilities = ":utilities"
+}
